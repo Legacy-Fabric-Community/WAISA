6655509366f47b8b7d78917568c5bc23198d2e7d
SD <harrypotter.starwars.shrish@gmail.com>
2022-09-17 17:05:25 +0530

    Initial rendering of box

--- src/main/java/io/github/boogiemonster1o1/waisa/client/WAISAClient.java
+++ src/main/java/io/github/boogiemonster1o1/waisa/client/WAISAClient.java
@@ -1,10 +1,15 @@
 package io.github.boogiemonster1o1.waisa.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+import net.legacyfabric.fabric.api.client.rendering.v1.HudRenderCallback;
+
+@Environment(EnvType.CLIENT)
 public class WAISAClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-
+		HudRenderCallback.EVENT.register(RenderManager.INSTANCE::render);
 	}
 }
